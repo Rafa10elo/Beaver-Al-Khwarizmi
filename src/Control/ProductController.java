b28581b221d743b051640b353a65720354d7292c
sarahZiadah <ziadahsarah@gmail.com>
2025-06-11 14:53:42 +0300

    added update functions for the price and quantity

--- src/Control/ProductController.java
+++ src/Control/ProductController.java
@@ -6,11 +6,26 @@ import Model.Product;
 public class ProductController {
     Avl<Product> products = new Avl<>();
 
-//    public void EditProductQuantity(int id,int quantity){
-//
-//        products.searchHelper();
-//
-//    }
+    public boolean updateProductPrice(int id, double newPrice) {
+        Avl<Product> avl = new Avl<>();
+        Product p = avl.searchHelper(id);
+        if (p == null)
+            return false;
+        if (newPrice >= 0)
+            p.setPrice(newPrice);
+        return true;
+    }
+
+    public boolean updateProductQuantity(int id, int newQuantity) {
+        Avl<Product> avl = new Avl<>();
+        Product p = avl.searchHelper(id);
+        if (p == null)
+            return false;
+        if (newQuantity >= 0 && newQuantity <= 1000)
+            p.setQuantity(newQuantity);
+        return true;
+    }
+
 
 
 
