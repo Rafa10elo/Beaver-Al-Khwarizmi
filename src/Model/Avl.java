e55fad99f9094539748d55a2e1a653082d038f30
sarahZiadah <ziadahsarah@gmail.com>
2025-06-11 14:51:32 +0300

    corrected the tree's balance in the insert function & corrected the search function & added a delete function & deleted some comments

--- src/Model/Avl.java
+++ src/Model/Avl.java
@@ -1,12 +1,11 @@
 package Model;
 
-import javax.crypto.KEM;
-import java.awt.event.KeyAdapter;
-import java.util.Optional;
+//import javax.crypto.KEM;
 
 public class Avl <T extends Comparable<T>> {
    public NODE root ;
-    class NODE {
+
+   class NODE {
         public T key;
         public int height;
         public NODE left, right;
@@ -18,8 +17,6 @@ public class Avl <T extends Comparable<T>> {
     }
 
     public void print(){
-
-
         System.out.println(root.right.key);
     }
 
@@ -27,150 +24,168 @@ public class Avl <T extends Comparable<T>> {
         root= null;
     }
 
-
-    int height(NODE node) {
-        // returns the height of the actual node and -1 for nulls
+    private int height(NODE node) {
         return (node == null) ? -1 : node.height;
     }
 
-    int getBalance(NODE node) {
-        //  this will return (the height of the left tree - the height of the right tree)
-        // if the node is null return 0  since both children are null (height -1)
+    private int getBalance(NODE node) {
         return (node == null) ? 0 : height(node.left) - height(node.right);
     }
 
-    NODE rightRotate(NODE y) {
-        //left child of the original node
+    private NODE rightRotate(NODE y) {
         NODE x = y.left;
-        // the right child of the left child of the original node
         NODE T2 = x.right;
 
-        // the right child of the left child of the original node is the original node
         x.right = y;
-        // the new left child of the original node (T2)
         y.left = T2;
 
-        // new heights
         y.height = Math.max(height(y.left), height(y.right)) + 1;
         x.height = Math.max(height(x.left), height(x.right)) + 1;
 
-        //this returns the left child (it will become the new root)
         return x;
     }
 
-    NODE leftRotate(NODE y) {
-        //right child of the original node
+    private NODE leftRotate(NODE y) {
         NODE x = y.right;
-        // the left child of the right child of the original node
         NODE T2 = x.left;
 
-        // the left child of the right child of the original node is the original node
         x.left = y;
-        // the new right child of the original node (T2)
         y.right = T2;
 
 
-        // new heights
         x.height = Math.max(height(x.left), height(x.right)) + 1;
         y.height = Math.max(height(y.left), height(y.right)) + 1;
 
-        //this returns the right child (it will become the new root)
         return x;
     }
 
-    public NODE search(NODE node,T key){
+    public Product searchHelper(int key){
+        Product product = new Product(key);
+        return search(root,key);
+    }
 
+    private Product search(NODE node,int key){
         if (node == null) return null;
-
-   int tony =  node.key.compareTo((T)key);
-        if (tony<0)
-            node.left = search(node.left, key);
-        else if ( tony > 0)
-            node.right = search(node.right, key);
+        Product current = (Product) node.key;
+        if (key == current.getProductID())
+            return current;
+        if (key < current.getProductID())
+            return search(node.left, key);
         else
-            return node;
-
-        return node;
-
-
+            return search(node.right, key);
     }
 
     public void insertHelper(T key){
         root = insert(root,key);
     }
-    public void searchHelper(Integer key){
 
-        Product product = new Product(key);
-        search(root,(T)key);
-
-
-
-    }
-
-  public NODE insert(NODE node, T key) {
-        //common insertion
+    private NODE insert(NODE node, T key) {
         if (node == null) return new NODE(key);
 
         int tony = key.compareTo(node.key);
 
-        if (tony > 0)
+        if (tony < 0)
             node.left = insert(node.left, key);
-        else if (tony < 0)
+        else if (tony > 0)
             node.right = insert(node.right, key);
         else
             return node;
 
-        //calculate the new height
         node.height = 1 + Math.max(height(node.left), height(node.right));
 
-        //the difference between the heights of the children
         int balance = getBalance(node);
-      int m1= 0;
-      int m2=0;
+      if (balance > 1 && key.compareTo(node.left.key) < 0)
+          return rightRotate(node);
 
-        //right rotation case
-        if(node.left!=null)
-            m1 = key.compareTo(node.left.key);
-        if(node.right!=null)
-            m2 = key.compareTo(node.right.key);
+      if (balance < -1 && key.compareTo(node.right.key) > 0)
+          return leftRotate(node);
 
-        if (balance > 1 && m1 > 0)
-            return rightRotate(node);
+      if (balance > 1 && key.compareTo(node.left.key) > 0) {
+          node.left = leftRotate(node.left);
+          return rightRotate(node);
+      }
 
-        //left rotation case
-        if (balance < -1 && m2 <= 0)
-            return leftRotate(node);
-
-        //left-right rotation case
-        if (balance > 1 && m1 <= 0) {
-            node.left = leftRotate(node.left);
-            return rightRotate(node);
-        }
-
-        //right-left rotation case
-        if (balance < -1 && m2 > 0) {
-            node.right = rightRotate(node.right);
-            return leftRotate(node);
-        }
+      if (balance < -1 && key.compareTo(node.right.key) < 0) {
+          node.right = rightRotate(node.right);
+          return leftRotate(node);
+      }
 
         return node;
-
-
     }
 
+    public void deleteHelper(int id) {
+        Product product = new Product(id);
+        root = delete(root, (T)product);
+    }
 
+    private NODE delete(NODE root, T key) {
+        if (root == null)
+            return null;
+
+        int tony = key.compareTo(root.key);
+        if (tony < 0)
+            root.left = delete(root.left, key);
+        else if (tony > 0)
+            root.right = delete(root.right, key);
+        else {
+            if ((root.left == null) || (root.right == null)) {
+                NODE temp ;
+                if(root.left!=null)
+                    temp=root.left;
+                else
+                    temp=root.right;
+
+                if (temp == null)
+                    return null;
+                else
+                    return temp;
+            }
+
+            NODE temp = getMinValueNode(root.right);
+            root.key = temp.key;
+            root.right = delete(root.right, temp.key);
+        }
+
+        root.height = Math.max(height(root.left), height(root.right)) + 1;
+
+        int balance = getBalance(root);
+
+        if (balance > 1 && getBalance(root.left) >= 0)
+            return rightRotate(root);
+
+        if (balance > 1 && getBalance(root.left) < 0) {
+            root.left = leftRotate(root.left);
+            return rightRotate(root);
+        }
+
+        if (balance < -1 && getBalance(root.right) <= 0)
+            return leftRotate(root);
+
+        if (balance < -1 && getBalance(root.right) > 0) {
+            root.right = rightRotate(root.right);
+            return leftRotate(root);
+        }
+
+        return root;
+    }
+
+    private NODE getMinValueNode(NODE node) {
+        NODE current = node;
+        while (current.left != null)
+            current = current.left;
+        return current;
+    }
 
     public void inorderHelper(){
         inorder(root);
     }
-    public void inorder(NODE root){
 
+    private void inorder(NODE root){
         if (root == null)
           return;
         inorder(root.left);
         System.out.println(root.key);
         inorder(root.right);
-
-
     }
+
 }
